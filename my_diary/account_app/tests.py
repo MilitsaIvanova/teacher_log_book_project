0ddb64f6a7e6c61c252abbdd149746ecb66c90f3
Militsa Ivanova <militsa8@abv.bg>
2023-08-11 21:12:41 +0300

    Added 12 test

--- my_diary/account_app/tests.py
+++ my_diary/account_app/tests.py
@@ -1,3 +1,124 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.core import exceptions
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import TeachersSubject
 
-# Create your tests here.
+from my_diary.account_app.models import validate_phone_number
+
+UserModel = get_user_model()
+
+
+class PhoneNumberValidatorTest(TestCase):
+
+    def test_valid_phone_number(self):
+        try:
+            validate_phone_number('123456')
+        except exceptions.ValidationError:
+            self.fail('Valid phone number was registered as invalid.')
+
+    def test_invalid_phone_number_letters(self):
+        with self.assertRaises(exceptions.ValidationError):
+            validate_phone_number('123456a')
+
+    def test_invalid_phone_number_special_chars(self):
+        with self.assertRaises(exceptions.ValidationError):
+            validate_phone_number('123456-123456')
+
+
+class CreateSubjectViewTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+
+        self.client = Client()
+
+        self.client.login(username='testuser', password='testpassword')
+
+        self.create_subject_url = reverse('create subject')
+
+    def test_create_subject(self):
+        data = {
+            'name': 'English',
+            'code': "10",
+            'description': 'some text'
+        }
+
+        response = self.client.post(self.create_subject_url, data)
+
+        subject = TeachersSubject.objects.first()
+        self.assertEqual(subject.name, 'English')
+        self.assertEqual(subject.code, '10')
+        self.assertEqual(subject.description, 'some text')
+
+        self.assertEqual(subject.teacher, self.user)
+
+        self.assertRedirects(response, reverse('classes', kwargs={'pk': self.user.pk}))
+
+
+class EditSubjectViewTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+
+        self.client = Client()
+        self.client.login(username='testuser', password='testpassword')
+
+        self.subject = TeachersSubject.objects.create(
+            teacher=self.user,
+            name="German",
+            code="20",
+            description="some text"
+        )
+
+        self.edit_subject_url = reverse('edit subject', kwargs={'pk': self.subject.pk})
+
+    def test_edit_subject(self):
+        data = {
+            'name': 'German',
+            'code': '20',
+            'description': 'some text'
+        }
+        response = self.client.post(self.edit_subject_url, data)
+
+        self.subject.refresh_from_db()
+
+        self.assertEqual(self.subject.name, 'German')
+        self.assertEqual(self.subject.code, '20')
+        self.assertEqual(self.subject.description, 'some text')
+
+        self.assertRedirects(response, reverse('classes', kwargs={'pk': self.user.pk}))
+
+
+class DeleteSubjectViewTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+
+        self.client = Client()
+        self.client.login(username='testuser', password='testpassword')
+
+        self.subject = TeachersSubject.objects.create(
+            teacher=self.user,
+            name="Spanish",
+            code="30",
+            description="some text"
+        )
+
+        self.delete_subject_url = reverse('delete subject', kwargs={'pk': self.subject.pk})
+
+    def test_delete_subject(self):
+        response = self.client.post(self.delete_subject_url)
+
+        self.assertFalse(TeachersSubject.objects.filter(pk=self.subject.pk).exists())
+
+        self.assertRedirects(response, reverse('classes', kwargs={'pk': self.user.pk}))
--- my_diary/students_and_groups/tests.py
+++ my_diary/students_and_groups/tests.py
@@ -1,3 +1,163 @@
-from django.test import TestCase
+from django.test import TestCase,Client
 
-# Create your tests here.
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from .models import Student, TeachersSubject, Group
+from .forms import AddStudentForm, EditStudentForm
+
+class StudentViewTests(TestCase):
+
+    def setUp(self):
+
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        self.client.login(username='testuser', password='testpassword')
+
+
+        self.subject = TeachersSubject.objects.create(
+            teacher=self.user,
+            name='English',
+            code='10',
+            description='some text',
+        )
+
+
+        self.student = Student.objects.create(
+            name='Gosho',
+            email='some@email.com',
+            teacher=self.user,
+            subject=self.subject
+        )
+
+    def test_add_student_view(self):
+        url = reverse('add student')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        data = {
+            'name': 'Gosho',
+            'email': 'some@email.com',
+            'contact_number': '1234567890',
+            'teacher': self.user.pk,
+            'place': 'some place',
+            'lesson_time': '12:00:00',
+            'more_information': 'some information',
+            'subject': self.subject.pk
+        }
+        response = self.client.post(url, data)
+        self.assertRedirects(response, f'/classes/{self.user.pk}')
+        self.assertEqual(Student.objects.count(), 2)
+
+    def test_edit_student_view(self):
+        url = reverse('edit student', kwargs={'pk': self.student.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+
+        updated_data = {
+            'name': 'Pesho',
+            'email': 'updated@email.com',
+            'contact_number': '0987654321',
+            'teacher': self.user.pk,
+            'place': 'Updated Place',
+            'lesson_time': '14:00:00',
+            'more_information': 'Updated information',
+            'subject': self.subject.pk
+        }
+        response = self.client.post(url, updated_data)
+        self.assertRedirects(response, f'/classes/{self.user.pk}')
+
+        self.student.refresh_from_db()
+        self.assertEqual(self.student.name, 'Pesho')
+        self.assertEqual(self.student.email, 'updated@email.com')
+
+    def test_delete_student_view(self):
+        url = reverse('delete student', kwargs={'pk': self.student.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(url)
+
+        self.assertRedirects(response, reverse('classes', kwargs={'pk': self.user.pk}))
+        self.assertEqual(Student.objects.count(), 0)
+
+class GroupViewTests(TestCase):
+
+    def setUp(self):
+
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        self.client.login(username='testuser', password='testpassword')
+
+
+        self.subject = TeachersSubject.objects.create(
+            teacher=self.user,
+            name='English',
+            code='10',
+            description='some text'
+        )
+
+        self.student = Student.objects.create(
+            name='Gosho',
+            email='some@email.com',
+            teacher=self.user,
+            subject=self.subject
+        )
+
+        self.group = Group.objects.create(
+            name='Test Group',
+            year=2023,
+            teacher=self.user,
+            subject=self.subject,
+        )
+
+    def test_create_group_view(self):
+        url = reverse('create group')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+
+        data = {
+            'name': 'New Group',
+            'year': 2023,
+            'teacher': self.user.pk,
+            'students': [self.student.pk],
+            'place':'some place',
+            'lesson_time': '14:00:00',
+            'subject': self.subject.pk,
+
+        }
+        response = self.client.post(url, data)
+        self.assertRedirects(response, reverse('groups', kwargs={'pk': self.user.pk}))
+        self.assertEqual(Group.objects.count(), 2)
+
+    def test_edit_group_view(self):
+        url = reverse('edit group', kwargs={'pk': self.group.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        updated_data = {
+            'name': 'Updated Group',
+            'year': 2024,
+            'teacher': self.user.pk,
+            'students': [self.student.pk],
+            'place': 'Updated Place',
+            'lesson_time': '15:00:00',
+            'subject': self.subject.pk,
+        }
+        response = self.client.post(url, updated_data)
+        self.assertRedirects(response, reverse('groups', kwargs={'pk': self.user.pk}))
+
+        self.group.refresh_from_db()
+        self.assertEqual(self.group.name, 'Updated Group')
+        self.assertEqual(self.group.year, 2024)
+
+    def test_delete_group_view(self):
+        url = reverse('group delete', kwargs={'pk': self.group.pk})
+        response = self.client.post(url)
+        self.assertRedirects(response, reverse('groups', kwargs={'pk': self.user.pk}))
+        self.assertEqual(Group.objects.count(), 0)
